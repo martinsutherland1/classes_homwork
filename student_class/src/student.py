ac252c26c71c68e74667b626406db4a9897265f4
Martin Sutherland <martinsutherland1@gmail.com>
2021-03-15 23:08:42 +0000

    completed homework - update

--- student_class/src/student.py
+++ student_class/src/student.py
@@ -3,18 +3,6 @@ class Student:
         self.name = name
         self.cohort = cohort
 
-    def test_student_has_name(self, name):
-        return self.name
-
-    def test_student_has_cohort(self, cohort):
-        return self.cohort
-
-    def test_student_can_update_name(self, name):
-        self.name = name
-
-    def test_student_can_update_chort(self, cohort):
-        self.cohort = cohort
-
     def talk(self):
         return ("I can talk!")
 
